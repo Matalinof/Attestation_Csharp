c8e49b1a980b25c1f36cd06954a8a57d9172c910
Надежда Старикова <nadezdastarikova@MacBook-Air-Nadezda.local>
2023-11-23 02:05:02 +0300

    Cделал задания по аттестации

---
+++ Task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+string Numbers(int a, int n)
+{
+    if (1<=n) return $"{n} "+ Numbers(1, n-1);
+    else return string.Empty;
+}
+
+System.Console.WriteLine(Numbers(1, 5));
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+//     else return string.Empty;
+// }
---
+++ Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая 
+найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int DiapasonSum(int start, int end)
+{
+    int count = 0;
+    for (int i = start; i <= end; i++)
+    {
+        count+=i;
+    }
+    return count;
+}
+System.Console.WriteLine("Введите начало диапазона");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите конец диапазона");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма натуральных чисел в диапазоне равна {DiapasonSum(a, b)}");
